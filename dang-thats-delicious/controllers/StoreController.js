fb6cb709c4cb133ed5688cee26a56eb92541c3c2
Sean Groff <sgroff04@gmail.com>
2017-12-08 07:23:57 -0600

    on to module 5

--- dang-thats-delicious/controllers/StoreController.js
+++ dang-thats-delicious/controllers/StoreController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   res.render('index', { title: 'Store' });
 };
@@ -6,6 +9,37 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  // Save to the DB
+  const store = await new Store(req.body).save();
+  req.flash(
+    'success',
+    `Successfully Created ${store.name}. Care to leave a review?`,
+  );
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  // Query the DB for a list of all Stores
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store given the id
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. Confirm they are the owner of the store
+  // 3. Render out the edit form so the user can update their store
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  // 1. Find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return new store instead of old store
+    runValidators: true, // enforce required fields, etc...
+  }).exec(); // some methods don't execute by default
+  // 2. Redirect them to the store and tell them it worked
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
+  res.redirect(`/stores/${store._id}/edit`);
 };
--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const storeController = require('../controllers/StoreController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
-router.get('/', storeController.homePage);
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+router.post('/add', catchErrors(storeController.createStore));
+router.post('/add/:id', catchErrors(storeController.updateStore));
 
 module.exports = router;
